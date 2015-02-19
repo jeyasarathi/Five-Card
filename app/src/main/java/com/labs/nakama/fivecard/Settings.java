0cade15793ad5840896519dc7d36f68744116c21
Jeyasarathi T S <sarathispa7@gmail.com>
2015-02-19 23:05:09 +0530

    Base code for Android Application.

---
+++ app/src/main/java/com/labs/nakama/fivecard/Settings.java
@@ -0,0 +1,77 @@
+package com.labs.nakama.fivecard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Settings extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+        new Thread(new Runnable() {
+            public void run() {
+
+                // Find the ListView resource.
+                ListView settingsListView = (ListView) findViewById(R.id.settingsListView);
+
+                // Create and populate a List of planet names.
+                String[] players = new String[] { "Change My Name", "Music"};
+                ArrayList<String> playersList = new ArrayList<>();
+                playersList.addAll( Arrays.asList(players) );
+
+                // Create ArrayAdapter using the planet list.
+                ArrayAdapter<String> listAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.settings_row, playersList);
+
+                // Add more planets. If you passed a String[] instead of a List<String>
+                // into the ArrayAdapter constructor, you must not add more items.
+                // Otherwise an exception will occur.
+                listAdapter.add("Player 3");
+                listAdapter.add("Player 4");
+
+                // Set the ArrayAdapter as the ListView's adapter.
+                settingsListView.setAdapter(listAdapter);
+
+                // When item is tapped, toggle checked properties of CheckBox and Planet.
+                settingsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View item,
+                                            int position, long id) {
+
+                        Intent intent;
+                        switch (position) {
+                            case 0:
+                                intent = new Intent(getApplicationContext(), ChangeName.class);
+                                startActivity(intent);
+                                break;
+                            default:
+                        }
+                    }
+                });
+            }
+        }).start();
+
+
+
+
+    }
+
+}
